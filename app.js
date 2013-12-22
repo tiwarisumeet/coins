ccb27a18a16875031ceb915f53401e831323f1ba
niktrix <nicky.gurbani@gmail.com>
2013-12-22 10:40:48 -0500

    added socket.io, pulling data from bitstamp

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-var express = require('express');
-var app = express();
-  var body ;
+ var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+var request = require('request');
+var json;
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-app.get('/', function(req, res){
+io.sockets.on('connection', function (socket) {
+ request('https://www.bitstamp.net/api/ticker/', function (error, response, body) {
+     console.log(body); 
+ socket.emit('news', body);
+});
   
-  var options = {
-  host: 'www.bitstamp.net',
-  path: '/api/ticker/'
-};
-var https = require('https');
-var req1 = https.get(options, function(res) {
-//   console.log('STATUS: ' + res.statusCode);
-//   console.log('HEADERS: ' + JSON.stringify(res.headers));
-  // Buffer the body entirely for processing as a whole.
-  var bodyChunks = [];
-  res.on('data', function(chunk) {
-    // You can process streamed parts here...
-    bodyChunks.push(chunk);
-  }).on('end', function() {
-     body = Buffer.concat(bodyChunks);
-    console.log('BODY: ' + body);
-    // ...and/or process the entire body here.
-  })
-});
-res.send(JSON.stringify(body));
-
-req.on('error', function(e) {
-  console.log('ERROR: ' + e.message);
-});
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP );
+
+
+server.listen(process.env.PORT || 3000, process.env.IP );
