c5275428dccfc7c1ea0649280000d27dd0a626f3
niktrix <nicky.gurbani@gmail.com>
2013-12-23 22:44:47 -0500

    realtime nvd3 graph update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
- var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
-var request = require('request');
-var json;
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+ var app = require('express')(),
+     server = require('http').createServer(app),
+     io = require('socket.io').listen(server);
+ var request = require('request');
+ var json;
+ app.get('/', function(req, res) {
+     res.sendfile(__dirname + '/index.html');
+ });
 
-app.get('/mtgox', function (req, res) {
-  res.sendfile(__dirname + '/testmtgox.html');
-});
+ app.get('/mtgox', function(req, res) {
+     res.sendfile(__dirname + '/testmtgox.html');
+ });
 
-io.sockets.on('connection', function (socket) {
- request('https://www.bitstamp.net/api/ticker/', function (error, response, body) {
-     console.log(body); 
- socket.emit('news', body);
-});
-  
-});
+ io.sockets.on('connection', function(socket) {
+     request('https://www.bitstamp.net/api/ticker/', function(error, response, body) {
+         console.log(body);
+         socket.emit('news', body);
+     });
+
+ });
 
 
 
-server.listen(process.env.PORT || 3000, process.env.IP );
+ server.listen(process.env.PORT || 3000, process.env.IP);
